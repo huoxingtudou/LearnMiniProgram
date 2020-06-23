ac5a3738f13e52bbe2d0890fa82d021b06ba35a7
huoxingtudou <1192706675@qq.com>
2020-06-23 14:54:53 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,28 @@
 // page/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    name:'yph',
+    age:18,
+    students:[
+      {id:1,name:'hhh',age:20},
+      {id:2,name:'yph',age:20},
+    ],
+    counter:0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleBtnClick(){
+    // 1.错误做法，界面不会刷新
+    // this.data.counter--;
+    // 2.this.setData()
+    this.setData({
+      counter:this.data.counter-1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  add(){
+    this.setData({
+      counter:this.data.counter+1
+    })
   }
 })
+// 编程范式
+// 1.命令式编程：原生DOM操作
+// 2.声明式编程：Vue
