9adcf7faf037e5a138480674e0e3e257696f93e8
huoxingtudou <1192706675@qq.com>
2020-06-24 14:57:11 +0800

    内置组件

---
+++ pages/input/input.js
@@ -0,0 +1,13 @@
+// pages/input/input.js
+Page({
+
+  handleInput(event){
+    console.log("用户输入内容"+event);
+  },
+  handleFocus(event){
+    console.log("input获取焦点"+event);
+  },
+  handleBlur(event){
+    console.log("input失去焦点"+event);
+  }
+})
